5f8afae6b8683f1b0e1ba035f6a501f1298d8368
miguelGuarrochena <mikeguarrochena@hotmail.com>
2026-03-29 16:50:31 +0200

    Add metrics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,17 @@
-import { ColorSchemeScript } from '@mantine/core';
-import { Space_Grotesk } from 'next/font/google';
+import { ColorSchemeScript } from "@mantine/core";
+import { Space_Grotesk } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
+import { ThemeProvider } from "@/components/ThemeProvider";
 
-import { ThemeProvider } from '@/components/ThemeProvider';
-
-import '@mantine/core/styles.css';
-import '@mantine/notifications/styles.css';
-import './globals.css';
+import "@mantine/core/styles.css";
+import "@mantine/notifications/styles.css";
+import "./globals.css";
 
 const spaceGrotesk = Space_Grotesk({
-  subsets: ['latin'],
-  display: 'swap',
-  weight: ['300', '400', '500', '600', '700'],
-  variable: '--font-space-grotesk',
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-space-grotesk",
 });
 
 export default function RootLayout({
@@ -26,11 +26,15 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         <meta name="google" content="notranslate" />
         <title>Miguel Guarrochena - Frontend Developer</title>
-        <meta name="description" content="Frontend Developer with experience in building responsive, high-performance web applications using React, Next.js, TypeScript, and modern CSS frameworks." />
+        <meta
+          name="description"
+          content="Frontend Developer with experience in building responsive, high-performance web applications using React, Next.js, TypeScript, and modern CSS frameworks."
+        />
       </head>
       <body className={spaceGrotesk.className}>
         <ThemeProvider>
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
